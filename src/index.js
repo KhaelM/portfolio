b69bcff45485009ede4dc34863920770505a69f1
Michael Randrianarisona <michael.randrianarisona@outlook.com>
2021-01-25 05:41:14 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Portfolio(props) {
+    return (
+        <div id="portfolio">
+            <nav>
+                <ul>
+                    <li className="active">home</li>
+                    <li>game development</li>
+                    <li>web development</li>
+                    <li>my story</li>
+                </ul>
+            </nav>
+            <div id="content-container">
+                <div id="home-container">
+                    <div id="main-info">
+                        <div className="name">michael</div>
+                        <div className="name">randrianarisona</div>
+                        <div id="job-title">software developer</div>
+                    </div>
+                    <div id="my-pic">
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Portfolio />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
